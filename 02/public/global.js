5b811bd01ec2741e9992459153a103d77c230185
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-12 13:28:21 -0500

    Completes exercise 2

--- 02/public/global.js
+++ 02/public/global.js
@@ -1,7 +1,9 @@
 var req = new XMLHttpRequest();
 
 // For 'Exercise 2', you will modify this line:
+// req.open("get", "/path1");
 req.open("get", "/path1");
+
 
 req.addEventListener("loadstart", function(){
   console.log("loadstart");
@@ -9,6 +11,12 @@ req.addEventListener("loadstart", function(){
 
 req.addEventListener("load", function(){
   // Your code for Exercise 1 goes here.
+  if (this.response == "yes") {
+    alert("yay!");
+  }
+  else {
+    alert("Aw, shucks!")
+  }
 });
 
 req.send();
