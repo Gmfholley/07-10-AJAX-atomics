453afb8381e102dc322cc486b95d1aff6b5c7431
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-12 13:28:09 -0500

    Completes Exercise 1

--- 01/public/global.js
+++ 01/public/global.js
@@ -1,7 +1,10 @@
 var req = new XMLHttpRequest();
 
 // For 'Exercise 2', you will modify this line:
-req.open("get", "/info1");
+// req.open("get", "/info1");
+//Now for Exercise 2, changing this line
+req.open("get", "/info2")
+
 
 req.addEventListener("loadstart", function(){
   console.log("loadstart");
@@ -9,6 +12,17 @@ req.addEventListener("loadstart", function(){
 
 req.addEventListener("load", function(){
   // Your code for Exercise 1 goes here.
+  
+  //Exercise 1 - alert should show containing the response.
+  // alert(this.response);
+  //Exercise 3 - alert should be yay if yes or aw shucks if not
+  if (this.response == "yes") {
+    alert("yay!");
+  }
+  else {
+    alert("Aw, shucks!")
+  }
+  
 });
 
 req.send();
