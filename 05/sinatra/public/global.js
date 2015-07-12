6e83e1b03e130fa8a4d4cd6f9261d9b6769725fb
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-12 14:47:22 -0500

    Completes assignment 5

--- 05/sinatra/public/global.js
+++ 05/sinatra/public/global.js
@@ -1,13 +1,20 @@
-
+// returns the id of the link (which is an Integer)
+//
+// returns an Integer
 function id_of_link(link){
   return link.getAttribute("data-task-id");
 }
 
-function mark_done(){
-  database_id = id_of_link(this);
-  a_link = this;
+
+
+function mark_done(event){
+  event.preventDefault();
   
-  done_request = new XMLHttpRequest();
+  var link = this;
+  var database_id = id_of_link(this);
+  var task = document.getElementById("task" + database_id);
+  
+  var done_request = new XMLHttpRequest();
   done_request.open("get", "/tasks/mark_as_done/" + database_id);
   
   done_request.addEventListener("loadstart", function(){
@@ -16,21 +23,28 @@ function mark_done(){
   
   done_request.addEventListener("load", function(){
     if (this.response == "Done!") {
-      a_link.classList.add("finished");
+      task.classList.add("finished");
+      link.innerHTML = "";
     }
     else {
       alert("Something happened.  Your change was not saved.  Try again.")
     }
   });
   
-  
+  done_request.send();
 }
+
+
+
+
+
+
+
 
 var finishLinks = document.getElementsByClassName("done_link");
 
 for (var i=0; i < finishLinks.length; i++) {
-  // finishLinks[i] represents each of the items in `finishLinks`.
-  
+  // finishLinks[i] represents each of the items in `finishLinks`. 
   finishLinks[i].addEventListener("click", mark_done);
   
 }
